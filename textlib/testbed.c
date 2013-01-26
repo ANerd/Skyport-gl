a9702b250ad76bd95e2c08b4f25298caa536b10a
jonathan <jwringstad@gmail.com>
2013-01-26 18:17:22 +0100

    added debug info

--- textlib/testbed.c
+++ textlib/testbed.c
@@ -18,12 +18,27 @@ int main(void){
   SDL_FillRect(mainsurf, NULL, SDL_MapRGB(mainsurf->format, 0, 0, 0));
   textlib_initialize();
   textlib_set_font_size(72);
-  textlib_set_background_color(0, 255, 255);
+  textlib_set_background_color(0, 0, 0);
   textlib_set_quality(2);
-  for(int i = 0;i<1000;i++){
-    SDL_Surface *text = textlib_get_text((char*)"foobar");
+  SDL_Surface *text = textlib_get_text((char*)"foobar");
+  printf("Text stats: \n\twidth: %d\n\theight: %d\n\talpha: %s\n\t"
+	 "scanline length: %d\n\tpixel format:\n"
+	 "\t\tbpp: %d\n"
+	 "\t\tred mask:\t%8x\n"
+	 "\t\tgreen mask:\t%8x\n"
+	 "\t\tblue mask:\t%8x\n"
+	 "\t\talpha mask:\t%8x\n",
+	 text->w, text->h, ((text->flags & SDL_SRCALPHA) > 0 ? "true" : "false"),
+	 text->pitch,
+	 text->format->BitsPerPixel,
+	 text->format->Rmask,
+	 text->format->Gmask,
+	 text->format->Bmask,
+	 text->format->Amask);
+  for(int i = 0;i<240;i++){
     SDL_BlitSurface(text, NULL, mainsurf, NULL); 
     SDL_UpdateRect(mainsurf, 0, 0, 0, 0);
+    SDL_Delay(16);
   }
   textlib_quit();
 }
